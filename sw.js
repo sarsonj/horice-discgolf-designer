4fffe85c76d24db8b68858a5a255f442a62620a6
Jindřich Šaršon <sarsonj@gmail.com>
2026-05-18 15:34:40 +0200

    Vyznačit soukromé parcely 2120/3 a 2120/4 (editor + viewer)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker pro Discgolf Designer
 // Strategie: app shell + runtime cache pro mapy
-const APP_VERSION = "v1.7.2";
+const APP_VERSION = "v1.7.3";
 const APP_CACHE = `discgolf-app-${APP_VERSION}`;
 const TILE_CACHE = 'discgolf-tiles-v1';
 
