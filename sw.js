a3dd45b00299e3a5de601d5ed80c6a0f9d44fd3a
Jindřich Šaršon <sarsonj@gmail.com>
2026-05-18 16:18:10 +0200

    update layoutu

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker pro Discgolf Designer
 // Strategie: app shell + runtime cache pro mapy
-const APP_VERSION = "v1.7.4";
+const APP_VERSION = "v1.7.5";
 const APP_CACHE = `discgolf-app-${APP_VERSION}`;
 const TILE_CACHE = 'discgolf-tiles-v1';
 
